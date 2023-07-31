229690ed94075f2821d159642ee9909dfb6b5d66
Thanuja <thanujadayananda05@gmail.com>
2023-07-31 15:41:31 +0530

    Merge remote changes and resolve conflicts

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,14 +1,15 @@
 import netflix from "../assets/svg/projects/netflix.svg"
 import weather from "../assets/svg/projects/weather.svg"
-
+import watch from "../assets/svg/projects/watch.png"
+import student from "../assets/svg/projects/student.png"
 export const projectsData = [
     {
         id: 1,
         projectName: 'NETFLIX-CLONE',
         projectDesc: 'This is a clone of popular streaming platform, Netflix',
         tags: ['React', 'JEST', 'Firebase'],
-        code: '',
-        demo: '',
+        code: 'https://github.com/ThanujaDayananda/Netflix-Clone',
+        demo: 'https://github.com/ThanujaDayananda/Netflix-Clone',
         image: netflix
     },
     {
@@ -19,6 +20,24 @@ export const projectsData = [
         code: 'https://github.com/ThanujaDayananda/Weather-App',
         demo: 'https://weather-app-bythanuja.netlify.app/',
         image: weather
+    },
+    {
+        id: 3,
+        projectName: 'TimeLapse',
+        projectDesc: 'Watch Shopping Application',
+        tags: ['HTML', 'CSS', 'JavaScript', 'React.js'],
+        code: 'https://github.com/ThanujaDayananda/TimeLapse-ShoppingCart',
+        demo: 'https://timelapse-shoppingcart.netlify.app/',
+        image: watch
+    },
+    {
+        id: 4,
+        projectName: 'Student Management System',
+        projectDesc: 'Student management system to store student details.',
+        tags: ['Python', 'MongoDB', 'TKinter'],
+        code: 'https://github.com/ThanujaDayananda/Student-Management-System',
+        demo: 'https://github.com/ThanujaDayananda/Student-Management-System',
+        image: student
     },
    
 ]
